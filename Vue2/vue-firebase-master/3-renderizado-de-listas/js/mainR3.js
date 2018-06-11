8b301cf91cfa58e8b5ea61c4fe4f34ef9fddb80a
raes88 <raes88@hotmail.com>
2018-06-11 11:46:24 +0200

    curso vue2 4-5

--- Vue2/vue-firebase-master/3-renderizado-de-listas/js/mainR3.js
+++ Vue2/vue-firebase-master/3-renderizado-de-listas/js/mainR3.js
@@ -1,7 +1,17 @@
-const vm= new Vue({
-	el:'main',
-	data:{
-		laborales:['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
-	},
+const vm = new Vue({
+    el: 'main',
+    data: {
+        laborales: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+        tareas: [
+            { nombre: 'Hacer la compra', prioridad: 'baja' },
+            { nombre: 'Aprender vue', prioridad: 'alta' },
+            { nombre: 'Ir al Gimnasio', prioridad: 'alta' },
+        ],
+        persona:{
+        	nombre:'Raul',
+        	profesion:'dev',
+        	ciudad:'Bilbao'
+        }
+    },
 
 })
