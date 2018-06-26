3d53709d8026c4b71a1a11a6b49b74ed17031489
raes88 <raes88@hotmail.com>
2018-06-26 12:25:12 +0200

    api rickand...

---
+++ rickandmortyvue/src/components/personaje.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="personajes">
+    <img :src="personaje.image" align="rigth">
+    <h5>id : {{personaje.id}}</h5>
+    <h5>Nombre : {{personaje.name}} </h5>
+    <h5> Origen: {{personaje.origin.name}}</h5>
+    <h5>Episodios : {{personaje.episode}} </h5>
+    <hr>
+  </div>
+</template>
+<script>
+export default {
+  name: 'personaje',
+  props: {
+    personaje: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  created () {
+    console.log(this.personaje)
+  }
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- rickandmortyvue/src/pages/Home.vue
+++ rickandmortyvue/src/pages/Home.vue
@@ -1,34 +1,38 @@
 <template>
   <div>
-    {{msg}}
-  </div>
-</template>
-
+    <personaje :personaje="personaje" v-for="personaje in personajes" :key="personaje._id"/>
+     <!-- <pre>{{$data}}</pre> -->
+ </div>
+ </template>
 <script>
 import axios from 'axios'
+import personaje from './../components/personaje.vue'
 export default {
   name: 'Home',
+  components: { personaje },
+  mounted () {
+    this.cargarPersonajes()
+  },
   data () {
     return {
-      msg: 'Vue Raul'
+      personajes: []
     }
   },
-  created () {
-    console.log('hola')
-    axios.get(`http://localhost:3000/consultas/1,183`)
-      .then(response => {
-        console.log(response.data)
-        // JSON responses are automatically parsed.
-        this.posts = response.data
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
+  methods: {
+    cargarPersonajes () {
+      console.log('hola')
+      axios.get(`http://localhost:3000/consultas/1,183`)
+        .then(respuesta => {
+          this.personajes = respuesta.data.resultado
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
   }
 }
 
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
