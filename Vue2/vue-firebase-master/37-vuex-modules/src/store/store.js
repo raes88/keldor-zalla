e847de85b71ffe7b20474e40858c5f9008e0098f
raes88 <raes88@hotmail.com>
2018-09-06 12:44:16 +0200

    222

--- Vue2/vue-firebase-master/37-vuex-modules/src/store/store.js
+++ Vue2/vue-firebase-master/37-vuex-modules/src/store/store.js
@@ -16,9 +16,10 @@ export const store = new Vuex.Store({
         totalCompra: (state) => state.carro.reduce((total, producto) => total + producto.precio, 0),
     },
     mutations: {
-        /*anadirProducto: (state, producto) => state.productos.unshift(producto),*/
+        anadirProducto: (state, producto) => state.productos.unshift(producto),
         comprarProducto: (state, indice) => state.carro.unshift(state.productos[indice]),
         eliminarProducto: (state, indice) => state.carro.splice(indice, 1),
+        //eliminarProductoLista: (state, producto) => state.productos.splice(indice, 1),
     },
     modules: {
         productos
