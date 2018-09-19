4bccd3fb5163449b0f19358a4794d88b53683abe
raes88 <raes88@hotmail.com>
2018-09-19 20:34:16 +0200

    home

---
+++ tienda virtual/tienda-front/src/components/producto.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="productos">
+    <img :src="producto.producImagen">
+    <h5> {{producto.producName}} </h5>
+    <h5> Descripcion : {{producto.producDescripcion}} </h5>
+    <h5> Categoria : {{producto.categoria}} </h5>
+    <h5> Precio : {{producto.producPrecio}}</h5>
+    <hr>
+  </div>
+</template>
+<script>
+export default {
+  name: 'producto',
+  props: {
+    producto: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  created () {
+    console.log(this.producto)
+  }
+}
+
+</script>
