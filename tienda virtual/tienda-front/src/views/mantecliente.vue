47c13abf9a7b29edd77f0b51c9c1099f6c0011cd
raes88 <raes88@hotmail.com>
2018-09-24 06:52:42 +0200

    clientemod

--- tienda virtual/tienda-front/src/views/mantecliente.vue
+++ tienda virtual/tienda-front/src/views/mantecliente.vue
@@ -1,20 +1,91 @@
 <template>
-  <div>
-    <h1>Hola {{$store.state.username}}, estatus de {{$store.state.role}}, email {{$store.state.email}}</h1>
+  <div> <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Home</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <button class="btn btn-outline-success my-2 my-sm-0 botn" type="button" v-on:click="manteProductos()">Productos</button>
+          <button class="btn btn-outline-success my-2 my-sm-0 botn" type="button" v-on:click="manteUsuarios()">Usuarios</button>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <h5 class="nav-item" align="right">Hola {{$store.state.username}}</h5>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <button type="button" class="btn btn-outline-success my-2 my-sm-0 botnLogin" v-on:click="salir()">Login out</button>
+        </form>
+      </div>
+    </nav>
     <h2>Manteniminto De Usuarios</h2>
-    <button type="button" class="btn btn-outline-success my-2 my-sm-0 botnLogin" v-on:click="volver()">Volver</button>
+    <div class="col-5 proUsu border border-dark resp">
+      <usuario :usuario="usuario" v-for="usuario in usuarios" :key="usuario._id" />
+    </div>
+    <button type="button" class="btn btn-outline-success my-2 my-sm-0" id=" botnVolver" v-on:click="volver()">Volver</button>
   </div>
 </template>
 <script>
+import axios from './../plugins/axios'
+import usuario from './../components/usuario.vue'
 export default {
-  name: 'Cliente',
+  name: 'MantenimintoUsuarios',
+  components: { usuario },
+  mounted () {
+    this.obtenerUsuarios()
+  },
+  data () {
+    return {
+      usuarios: []
+    }
+  },
   methods: {
     volver () {
       this.$router.push({ name: 'admin' })
+    },
+    obtenerUsuarios () {
+      axios.get('http://localhost:4040/api/users?limit=2')
+        .then((respuesta) => {
+          this.usuarios = respuesta.data
+          console.log(respuesta)
+        })
+        .catch(e => {
+          // alert('Rellena todos los campos')
+          console.log(e)
+        })
     }
   }
 }
+
 </script>
 <style scoped>
+.botnLogin {
+  margin-left: 6px;
+}
+
+.botn {
+  margin-right: 8px;
+}
+
+h5 {
+  margin-right: 15px;
+}
+.proUsu{
+  padding: 3%;
+  margin: 3%;
+}
+#respuesta{
+  margin-top: 2%;
+  margin-right: 2%;
+  background: #E4E4E4;
+}
+.resp{
+  background: #F2F2F2;
+}
 
 </style>
---
+++ tienda virtual/tienda-front/src/views/modificarusuario.vue
@@ -0,0 +1,238 @@
+<template>
+  <div class="container">
+    <div class="row h-100 justify-content-center align-items-center">
+      <form class="col-12">
+        <div class="d-flex justify-content-center">
+          <div class="shadow p-3 mb-5 bg-light rounded" id="registro">
+            <h1 class="d-flex justify-content-center"><p class="font-weight-bold">Registro</p></h1>
+            <div id="campos">
+              <fieldset class="shadow p-3 mb-5 bg-light rounded">
+                <div class="form-group">
+                  <div class="d-flex justify-content-around">
+                    <label for="formGroupExampleInput">Nombre:</label>
+                    <input type="text" class="form-control nombre" name="username" v-model="input.username" placeholder="Nombre" required />
+                    <label for="formGroupExampleInput">Password:</label>
+                    <input id="password" class="form-control pass" type="password" name="password" v-model="input.password" placeholder="Password" required />
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset id="direccion" class="shadow p-3 mb-5 bg-light rounded">
+                <div class="form-group">
+                  <h3 class="d-flex justify-content-center">
+                <p class="font-weight-bold">Direccion</p>
+              </h3>
+                  <div class="form-group">
+                    <div class="d-flex justify-content-around">
+                      <label for="formGroupExampleInput">Calle:</label>
+                      <input id="calle" class="form-control" type="text" name="calle" v-model="input.direccion.calle" placeholder="Calle" required/>
+                      <label for="formGroupExampleInput">Num:</label>
+                      <input id="direcNum" class="form-control" type="number" name="numero" v-model="input.direccion.numero" placeholder="Num" required/>
+                    </div>
+                    <div class="d-flex justify-content-around ">
+                      <label for="formGroupExampleInput">Cp:</label>
+                      <input id="direcCp" class="form-control" type="number" name="cp" v-model="input.direccion.cp" placeholder="CP" required/>
+                      <label for="formGroupExampleInput">Local.:</label>
+                      <input id="direcText" class="form-control" type="text" name="localidad" v-model="input.direccion.localidad" placeholder="Localidad" required/>
+                      <label for="formGroupExampleInput">Prov.:</label>
+                      <input id="direcText" class="form-control" type="text" name="provincia" v-model="input.direccion.provincia" placeholder="Provincia" required/>
+                    </div>
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset class="shadow p-3 mb-5 bg-light rounded">
+                <div class="d-flex justify-content-around">
+                  <label for="formGroupExampleInput">Telf:</label>
+                  <input id="tel" class="form-control" type="tel" name="telefono" v-model="input.mobileNumber" placeholder="Telefono" required />
+                  <label for="formGroupExampleInput">Email:</label>
+                  <input id="email" class="form-control" type="text" name="email" v-model="input.email" placeholder="Email" required/>
+                  <label for="formGroupExampleInput">Role:</label>
+                  <select id="role" class="form-control" v-model="input.role" required>
+                    <option value="CLIENT">Cliente</option>
+                    <option value="ADMIN">Adminstrador</option>
+                  </select>
+                </div>
+              </fieldset>
+              <div id="botones" class="d-flex justify-content-center">
+                <button type="button" class="btn btn-outline-success my-2 my-sm-0" v-on:click="altaUsuario()">Alta De Usuario</button>
+                <button type="reset" class="btn btn-outline-success my-2 my-sm-0">Resetear Formulario</button>
+                <button type="button" class="btn btn-outline-success my-2 my-sm-0" v-on:click="salir()">Salir</button>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import axios from './../plugins/axios'
+export default {
+  name: 'registro',
+  data () {
+    return {
+      input: {
+        username: '',
+        password: '',
+        mobileNumber: '',
+        email: '',
+        role: '',
+        direccion: {
+          calle: '',
+          cp: '',
+          numero: '',
+          provincia: '',
+          localidad: ''
+        }
+
+      }
+    }
+  },
+  methods: {
+    altaUsuario () {
+      axios.post('http://localhost:4040/api/users ', this.input)
+        .then((respuesta) => {
+          console.log(respuesta)
+          if (respuesta.status === 200) {
+            this.$store.dispatch('login', this.input)
+          } else {
+            console.log('error')
+          }
+        })
+        .catch(e => {
+          alert('Rellena todos los campos')
+          console.log(e)
+        })
+    },
+    salir () {
+      this.$router.push({ name: 'login' })
+    }
+  }
+}
+
+</script>
+<style scoped>
+label {
+  margin: 7px;
+}
+
+#direcNum {
+  margin-bottom: 20px;
+  width: 150px;
+}
+
+button {
+  margin-right: 10px;
+}
+
+#password {
+  width: 250px;
+}
+#direcCp{
+  width:14%;
+}
+#direcText{
+  width:35%;
+}
+#role{
+  width:35%;
+}
+#tel{
+  width:25%;
+}
+.nombre{
+  width:50%;
+}
+.pass{
+  width:35%;
+}
+
+form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.container {
+ position: absolute;
+ left: 18%;
+ top: 3%;
+ width: 70%;
+ height: 70%px;
+/* margin-top: -300px;
+ margin-left: -475px;*/
+}
+
+/*
+#registro {
+  align-content: center;
+#FFFFFF  border: 1px solid #212221;
+  background-color: #433A3A;
+  margin: auto;
+  margin-top: 80px;
+  padding: 20px;
+  margin-right: 240spx;
+}
+
+#campos {
+  display: inline;
+  width: 680px;
+  margin-top: 10px;
+}
+
+#titulos {
+  margin-top: 4px;
+  margin-bottom: 2px;
+  text-align: left;
+  padding-left: 12px;
+}
+
+#botones {
+  margin-top: 10px;
+  text-align: center;
+}
+
+input {
+  width: 500px;
+  margin-bottom: 8px;
+  margin-top: 8px;
+}
+
+select {
+  width: 480px;
+}
+
+#direcNum {
+  width: 50px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+
+#calle {
+  width: 300px;
+  margin-top: 8px;
+}
+
+#direcText {
+  width: 122px;
+  margin-top: 8px;
+}
+
+#direccion {
+  align-content: center;
+}
+
+#role {
+  width: 100px;
+}
+
+#texto {
+  width: 450px;
+}
+
+fieldset {
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}*/
+
+</style>
