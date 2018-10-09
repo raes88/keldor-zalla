c03294b415d6eb322eb830d25f8055075ec04fd5
raes88 <raes88@hotmail.com>
2018-10-09 12:46:01 +0200

    test2

--- GildedRose-Refactoring-Kata-master/js/spec/gilded_rose_spec.js
+++ GildedRose-Refactoring-Kata-master/js/spec/gilded_rose_spec.js
@@ -1,22 +1,5 @@
 describe("Gilded Rose", function() {
-    it("should foo", function() {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("foo");
-    })
-
-
-    const sumar = function(a, b) {
-        return a + b
-    }
-
-    describe("funcion sumar", function() {
-        it("debe sumar correctamente dos valores", function() {
-            const suma = sumar(2, 2)
-            expect(suma).toEqual(4)
-        })
-    })
-
+  
     describe(" Item 'Aged brie'", function() {
         it("Incrementar la calidad del queso en uno", function() {
             const gildedRose = new Shop([new Item("Aged Brie", 15, 2)]);
